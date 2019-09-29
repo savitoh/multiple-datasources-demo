fac08b2cb56e2d58d76a8f372745895387ff5d8b
savitoh <savioraires10@gmail.com>
2019-09-29 20:10:08 -0300

    First Commit

---
+++ src/main/java/com/savitoh/multipledatasourcesdemo/MultipleDatasourcesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.savitoh.multipledatasourcesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDatasourcesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleDatasourcesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savitoh/multipledatasourcesdemo/MultipleDatasourcesDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.savitoh.multipledatasourcesdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultipleDatasourcesDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
